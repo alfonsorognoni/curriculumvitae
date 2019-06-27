331959c84161526e975c7b489f1c9e86fc91b333
Alfonso Rognoni <alfonsorognoni@gmail.com>
2019-06-27 07:19:11 -0300

    styled components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,82 @@
 //presentacional solo muestra 
 import React from 'react';
 import Social from './Social';
+import styled from 'styled-components';
+
+const AboutStyled = styled.div`
+    text-align: center;
+`;
+
+const AboutAvatar = styled.div`
+    padding: 2em 0 0 0;
+`;
+
+const AboutImg = styled.img`
+    border-radius: 100%;
+    width: 160px;
+    height: 160px;
+    border: 2px solid blue;
+    margin: 0 auto;
+    display: block;
+    box-shadow: 0 0 10px rgba(0, 0, 0, .6);
+`;
+
+const AboutName = styled.div`
+    text-align: center;
+`;
+
+const AboutH2 = styled.h2`
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    letter-spacing: 1.2px;
+    margin: .5em 0 0 0;
+    color: blue;
+`;
+
+const AboutProfession = styled.p`
+    margin: .2em 0 1em 0;
+    letter-spacing: 1.6px;
+    font-weight: 300;
+    color: blue;
+`;
+
+const AboutBio = styled.p`
+    font-weight: 300;
+    font-size: 1em;
+    color: gray;
+`;
+
+const AboutLocation = styled.p`
+    font-weight: 400;
+    font-size: 1em;
+    color: gray;
+`;
 
 const About = ({ avatar, name, profession, bio, address, social}) => (
-    <div className="About">
+    <AboutStyled>
         <div className="About-container">
-            <div className="About-avatar">
+            <AboutAvatar>
                 <figure>
-                    <img src={avatar} alt={name}/>
+                    <AboutImg src={avatar} alt={name}/>
                 </figure>
-            </div>
-            <div className="About-name">
-                <h2>{name}</h2>
-            </div>
+            </AboutAvatar>
+            <AboutName>
+                <AboutH2>{name}</AboutH2>
+            </AboutName>
             <div className="About-profession">
-                <p>{profession}</p>
+                <AboutProfession>{profession}</AboutProfession>
             </div>
             <div className="About-desc">
-                <p>{bio}</p>
+                <AboutBio>{bio}</AboutBio>
             </div>
             <div className="About-location">
-                <p>{address}</p>
+                <AboutLocation>{address}</AboutLocation>
             </div>
             <div className="About-social">
                 <Social social={social} />
             </div>
         </div>
-    </div>
+    </AboutStyled>
 )
 
 export default About;
